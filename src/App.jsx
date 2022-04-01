a403c98f473ec81389d2972848698cbdb157f9ee
felipe maya <83172202+felipemaya4@users.noreply.github.com>
2022-04-01 06:44:18 +0000

    ejercicio 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './App.css';
 
-const Header =(props)=>{
+const Header =(props)=>{ // creacion de components
     return(
         <>
             <h1>{props.course}</h1>
@@ -10,12 +10,22 @@ const Header =(props)=>{
     )
 }
 
-const Content =(props)=>{
+const Part =(props)=>{
     return(
         <>
             <p>
                 {props.part}   {props.exercises}
             </p>
+        </>
+    )
+}
+
+const Content =(props)=>{ // un componente utilizando otro componente y paandole atributos
+    return(
+        <>
+            <Part part={props.part1} exercises={props.exercises1}/>
+            <Part part={props.part2} exercises={props.exercises2}/>
+            <Part part={props.part3} exercises={props.exercises3}/>
         </>
     )
 }
@@ -42,9 +52,7 @@ const App =()=>{
     return (
         <>
             <Header course ={course} />
-            <Content part={part1} exercises={exercises1} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
             <Total total={exercises1+exercises2+exercises3} />
         </>
     )
